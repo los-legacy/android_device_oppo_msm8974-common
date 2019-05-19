7146820caf470ab1307073271baf1221887df16b
Feinzer <danhernan23i@gmail.com>
2019-05-19 15:49:41 +0000

    msm8974-common: Remove Trust HAL

--- msm8974.mk
+++ msm8974.mk
@@ -241,10 +241,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oppo_msm8974
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.bacon
